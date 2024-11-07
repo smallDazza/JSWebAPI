16358ce9c24fff44cf000783aac20d8fcedf79da
smallDazza <smalldazza@gmail.com>
2024-11-07 20:14:32 +1100

    Feat: added pokemon api data

---
+++ src/pokemonEncounter.js
@@ -0,0 +1,34 @@
+console.log("Pokemon journey begins");
+
+const encounterButton = document.getElementById("pokemonEncounterButton")
+
+async function getPokemon(){
+    console.log("Looking for a wild Pokemon");
+
+    let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/squirtle");
+    let apiData = await apiResponse.json();
+
+    // Fetch name, type, image, cry
+    // let pokemonName = apiData.name;
+    return {
+        name: apiData.name,
+        types: apiData.types,
+        image: apiData.sprites.other.home.front_default,
+        sound: apiData.cries.latest
+    };
+
+    return pokemonName;
+}
+
+// encounterButton.addEventListener("click", (event) => getPokemon(event));
+
+// encounterButton.addEventListener("click", getPokemon);
+
+encounterButton.addEventListener("click",async (event) => {
+    console.log("Doing something...");
+
+    let pokemonResult = await getPokemon();
+    
+    
+    console.log(pokemonResult);
+});
